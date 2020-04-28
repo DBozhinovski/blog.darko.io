37d026dc1ea39dde2ed0dc1a1bc725837314edde
Darko Bozhinovski <dbozhinovski@gmail.com>
2020-04-28 14:19:14 +0200

    Adds rona post

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,8 @@
 'use strict';
 
 require('./src/assets/scss/init.scss');
-require('./static/css/prismjs/theme.min.css');
+// require('./static/css/prismjs/theme.min.css');
+require('prismjs/plugins/command-line/prism-command-line.css');
+require('prismjs/themes/prism-coy.css');
+require('prismjs/plugins/line-numbers/prism-line-numbers.css');
+require('./static/css/prismjs/fontsize.css');
